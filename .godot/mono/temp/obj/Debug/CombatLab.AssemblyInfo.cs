6e4cb052b91dd86e3bac1160af823d4deb02897c
koliruslik <koliruslik@gmail.com>
2026-05-13 04:12:46 +0300

    feat: add parallaxed background, level design
    refactor: organize scenes and resources into proper folders

--- .godot/mono/temp/obj/Debug/CombatLab.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/CombatLab.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CombatLab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf4d6d388fa129b320246d8dff111feb4b1baaed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3fbf3f71d8f7888d45c8da4c2d3bc8ad356fda4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CombatLab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CombatLab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
